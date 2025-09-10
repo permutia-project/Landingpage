77802fc881a317ec5ab0e01d786f4c8094c2695a
Joel Brenner <joelbrenner@MacBook-Air-von-Joel.local>
2025-09-10 12:27:05 +0200

    Logo verkleinert und zentriert auf Startseite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
 import logo from '../assets/Logo.png';
-import bundeslaenderImg from '../assets/bundeslaender.jpg';
 
-export default function Home() {
+function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <img src={logo} alt="Permutia Logo" className="w-48 mb-4" />
-      <h1 className="text-3xl font-bold mb-6 text-center">Permutia – Lehrer:innen im Bundesländer-Austausch</h1>
-      <img
-        src={bundeslaenderImg}
-        alt="Übersicht der Bundesländer"
-        className="max-w-full h-auto rounded shadow"
-      />
+    <div className="text-center">
+      <img src={logo} alt="Permutia Logo" className="w-48 h-auto mx-auto my-4" />
+      <h1 className="text-3xl font-bold mt-4">Permutia – Lehrer:innen im Bundesländer-Austausch</h1>
     </div>
   );
 }
+
+export default Home;
