563f1cced7554b58d381499a84d1606463e79517
Joel Brenner <joelbrenner@MacBook-Air-von-Joel.local>
2025-09-10 12:32:27 +0200

    Logo auf 1/16 Größe reduziert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/Logo.png';
 function Home() {
   return (
     <div className="text-center">
-      <img src={logo} alt="Permutia Logo" className="w-48 h-auto mx-auto my-4" />
+      <img src={logo} alt="Permutia Logo" className="w-16 h-auto mx-auto my-4" />
       <h1 className="text-3xl font-bold mt-4">Permutia – Lehrer:innen im Bundesländer-Austausch</h1>
     </div>
   );
