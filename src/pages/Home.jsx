ec7b192c4ab435013d90a9efabc823e0344e1b2e
Joel Brenner <joelbrenner@MacBook-Air-von-Joel.local>
2025-09-10 12:36:19 +0200

    Logo-Import korrigiert und verkleinert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-import logo from '../assets/Logo.png';
+import React from 'react';
+import logo from '../assets/Logo.png'; // Großgeschrieben!
 
 function Home() {
   return (
     <div className="text-center">
-      <img src={logo} alt="Permutia Logo" className="w-16 h-auto mx-auto my-4" />
-      <h1 className="text-3xl font-bold mt-4">Permutia – Lehrer:innen im Bundesländer-Austausch</h1>
+      <img
+        src={logo}
+        alt="Permutia Logo"
+        className="w-16 h-auto mx-auto my-4"
+        style={{ maxWidth: '64px' }} // zusätzlich zur Absicherung
+      />
+      <h1 className="text-3xl font-bold mt-4">
+        Permutia – Lehrer:innen im Bundesländer-Austausch
+      </h1>
     </div>
   );
 }
