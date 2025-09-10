32ceb04982ffcd6b81d0bc4ea73e44b947304e5f
Joel Brenner <joelbrenner@MacBook-Air-von-Joel.local>
2025-09-10 12:22:32 +0200

    Fix: Groß-/Kleinschreibung im Bildnamen korrigiert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 import bundeslaenderImg from '../assets/bundeslaender.jpg';
 
 export default function Home() {
